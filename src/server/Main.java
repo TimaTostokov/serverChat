707c5b3ce95076c6747284048c419113f776cdbf
Tolik <t1005828@gmail.com>
2024-01-11 17:48:20 +0300

    Initial commit

---
+++ src/server/Main.java
@@ -0,0 +1,9 @@
+package server;
+
+public class Main {
+    public static void main(String[] args) {
+        ClientWindow clientWindow = new ClientWindow();
+        Server server = new Server();
+    }
+
+}
